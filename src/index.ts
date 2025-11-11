659aefc1b7debd4ecb37b86cdb24603fd78e81f2
madsnedergaard <nedergaardmads@gmail.com>
2025-11-12 00:40:10 +0100

    Uses context to get ref and issue

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
+import { context } from '@actions/github';
+
 import { getPreviewData, findLinearIdentifierInComment } from './github';
 import { getLinearIssueId, setAttachment } from './linear';
 
 async function main() {
-    // TODO: The ref should be the pull request's branch or commit hash,
-    // we need to get this from the action context
-    const gitRef = 'c03ad5a3605e674bd61e65b4a72eada35c886b27';
-    const ghIssueNumber = 3;
+    console.log(context);
+    const gitRef = context.ref;
+    if (!gitRef) {
+        console.error('No git ref found');
+        throw new Error('No git ref found');
+    }
+    const ghIssueNumber = context.issue.number;
     const previewData = await getPreviewData(gitRef);
-
-    // TODO: Get the actual GH issue number from context?
     const linearIdentifier = await findLinearIdentifierInComment(ghIssueNumber);
-
     const issue = await getLinearIssueId(linearIdentifier);
+
     // TODO: Can we get the PR title from context?
     await setAttachment({
         issueId: issue.id,
